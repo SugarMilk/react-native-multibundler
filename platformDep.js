4bcab9a18ae507cfe0844f7f597b39c756e2db72
huangjian <1776549643@qq.com>
2020-08-10 16:32:05 +0800

    m

--- platformDep.js
+++ platformDep.js
@@ -1,28 +1,24 @@
-import {AppRegistry,DeviceEventEmitter,View,Platform} from 'react-native';
+import {AppRegistry, DeviceEventEmitter, View, Platform} from 'react-native';
 import 'react';
 import 'react-native';
-import React,{Component} from "react";
+import React, {Component} from "react";
 
 import {SmartAssets} from "react-native-smartassets";
 SmartAssets.initSmartAssets();
-DeviceEventEmitter.addListener('sm-bundle-changed',
-	(bundlePath)=>{
-		SmartAssets.setBundlePath(bundlePath);
-	});
+DeviceEventEmitter.addListener('sm-bundle-changed', (bundlePath) => {
+  SmartAssets.setBundlePath(bundlePath);
+});
 
-import { NativeEventEmitter, NativeModules } from 'react-native';
-if(Platform.OS != 'android') {//ios
-	const {BundleloadEventEmiter} = NativeModules;
+import {NativeEventEmitter, NativeModules} from 'react-native';
+if (Platform.OS != 'android') { //ios
+  const {BundleloadEventEmiter} = NativeModules;
 
-	const bundleLoadEmitter = new NativeEventEmitter(BundleloadEventEmiter);
+  const bundleLoadEmitter = new NativeEventEmitter(BundleloadEventEmiter);
 
-	const subscription = bundleLoadEmitter.addListener(
-		'BundleLoad',
-		(bundleInfo) => {
-			console.log('BundleLoad==' + bundleInfo.path);
-			SmartAssets.setBundlePath(bundleInfo.path);
-		}
-	);
+  const subscription = bundleLoadEmitter.addListener('BundleLoad', (bundleInfo) => {
+    console.log('BundleLoad==' + bundleInfo.path);
+    SmartAssets.setBundlePath(bundleInfo.path);
+  });
 }
 
 require('react-native/Libraries/Core/checkNativeVersion');
